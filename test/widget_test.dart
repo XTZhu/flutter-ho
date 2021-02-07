87fbd5b61123023288b30768a9cef4d3a6de561b
zhaolong <928343994@qq.com>
2021-02-07 18:19:46 +0800

    GridView Demo

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,21 +13,19 @@ void main() {
     print("测试数据");
 
     //创建一个数组
-    List<int> testList = [];
+    List<int> testList = [1,2,3,1,2,1,1,4,5,6,2,34,];
 
-    //添加数据
-    testList.add(2);
-    testList.add(2);
-    testList.add(2);
-    testList.add(2);
 
-    print("测试数据 - $testList");
+    Map<String,List<int>> map = Map();
 
-    //插入数据 在0号位置插入一个数据
-    testList.insert(0, 33);
-
-    print("测试数据 - $testList");
-
+    testList.forEach((element) {
+      if(map.containsKey("$element")){
+        List<int> valueList = map["$element"];
+        valueList.add(element);
+      }else{
+        map["$element"]=[element];
+      }
+    });
 
 
 
